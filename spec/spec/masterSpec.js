97308815865012d2ca87969e20167c812061155d
Sergey Andronnikov <sergey.andronnikov@altexsoft.com>
2017-06-19 18:34:38 +0300

    Jasmine Integration

--- spec/spec/masterSpec.js
+++ spec/spec/masterSpec.js
@@ -1,12 +1,16 @@
 describe("Master", function() {
     // init environment
     const qoper8 = require('ewd-qoper8');
-    var master = new qoper8.masterProcess();
-    master.log = false;
-
     jasmine.clock().install();
 
     describe("VERSION", function () {
+        var master;
+        beforeEach(function() {
+            master = new qoper8.masterProcess();
+            master.log = false;
+            master.shutdownDelay = 1;
+        });
+
         it ("should be not empty string", function () {
             var version = master.version();
             expect(version).toBeNonEmptyString();
@@ -14,6 +18,17 @@ describe("Master", function() {
     });
 
     describe("QUEUE", function () {
+        var master;
+        beforeEach(function() {
+            master = new qoper8.masterProcess();
+            master.log = false;
+            master.shutdownDelay = 1;
+        });
+
+        afterEach(function () {
+            //
+        });
+
         it ("should have control methods", function () {
             var spy1 = spyOn(master, 'start');
             expect(master.start).toBeDefined();
@@ -24,40 +39,43 @@ describe("Master", function() {
             expect(master.stop).toBeDefined();
             master.stop();
             expect(spy2).toHaveBeenCalled();
-        });
-
+        }, 300);
 
         it ("Should invoke 'start' and 'started' event", function () {
             var spy = spyOn(master, "emit");
             master.start();
             expect(spy).toHaveBeenCalledWith("start");
             expect(spy).toHaveBeenCalledWith("started");
+            jasmine.clock().tick(100);
+
             setTimeout(function() {
                 master.stop();
             }, 100);
         }, 300);
 
-        // it ("Should be possible to add messages to queue", function() {
-        //     master.on('started', function () {
-        //         var messageObj = {
-        //             type: 'testMessage1',
-        //             hello: 'world'
-        //         };
-        //         master.addToQueue(messageObj);
-        //
-        //         messageObj = {
-        //             type: 'testMessage2',
-        //             hello: 'another world'
-        //         };
-        //         master.addToQueue(messageObj);
-        //     });
-        //     master.start();
-        //     // jasmine.clock().tick(50);
-        //     // console.log('---', master.queue.length);
-        //     setTimeout(function () {
-        //         master.stop();
-        //     }, 100);
-        // }, 300);
+        it ("Should be possible to add messages to queue", function() {
+            master.on('started', function () {
+                var messageObj = {
+                    type: 'testMessage1',
+                    hello: 'world'
+                };
+                this.addToQueue(messageObj);
+
+                messageObj = {
+                    type: 'testMessage2',
+                    hello: 'another world'
+                };
+                this.addToQueue(messageObj);
+            });
+            master.start();
+
+            expect(master).toHaveNonEmptyArray('queue');
+            expect(master.queue).toBeArrayOfSize(2);
+
+            setTimeout(function () {
+                master.stop();
+            }, 100);
+        }, 300);
 
 
     });
